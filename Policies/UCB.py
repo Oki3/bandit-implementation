6d405afd5966f79e52f254755aadc9a2933982be
Weicheng Hu <76918682+Oki3@users.noreply.github.com>
2024-05-30 13:21:18 +0200

    modify main

--- Policies/UCB.py
+++ Policies/UCB.py
@@ -6,16 +6,17 @@ class UCB:
         self.alpha = alpha
         self.counts = np.zeros(n_arms)
         self.values = np.zeros(n_arms)
+        self.total_counts = 0
     
     def select_arm(self):
-        total_counts = np.sum(self.counts)
-        if total_counts < self.n_arms:
-            return int(total_counts)
-        ucb_values = self.values + self.alpha * np.sqrt(np.log(total_counts + 1) / (self.counts + 1e-5))
+        if self.total_counts < self.n_arms:
+            return int(self.total_counts)
+        ucb_values = self.values + self.alpha * np.sqrt((np.log(self.total_counts)) / (self.counts))
         return int(np.argmax(ucb_values))
     
     def update(self, chosen_arm, reward):
         self.counts[chosen_arm] += 1
+        self.total_counts += 1
         n = self.counts[chosen_arm]
         value = self.values[chosen_arm]
         new_value = ((n - 1) / n) * value + (1 / n) * reward
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from Policies import UCB, EXP3, LinUCB
+from Policies.UCB import UCB
+from Policies.EXP3 import EXP3
+from Policies.LinUCB import LinUCB
 
 # Given parameters
 w = np.array([0.44, 0.6, 0.2])
@@ -22,55 +24,76 @@ def calculate_reward(t, context):
     reward_vector = calculate_reward_vector(t)
     return np.dot(reward_vector, context)
 
-# Simulation
-num_iterations = 10000
-ucb_rewards = []
-exp3_rewards = []
-linucb_rewards = []
+# Simulation parameters
+num_iterations = 100000
+num_repetitions = 1
 
-ucb = UCB(n_arms=len(context_vectors))
-exp3 = EXP3(n_arms=len(context_vectors))
-linucb = LinUCB(n_arms=len(context_vectors), d=len(context_vectors[0]))
+# Initialize reward arrays
+ucb_rewards = np.zeros(num_iterations)
+exp3_rewards = np.zeros(num_iterations)
+linucb_rewards = np.zeros(num_iterations)
 
-for t in range(1, num_iterations + 1):
-    # UCB
-    ucb_arm = ucb.select_arm()
-    ucb_context = context_vectors[ucb_arm]
-    ucb_reward = calculate_reward(t, ucb_context)
-    ucb.update(ucb_arm, ucb_reward)
-    if t == 1:
-        ucb_rewards.append(ucb_reward)
-    else:
-        ucb_rewards.append(ucb_rewards[-1] + ucb_reward)
+# Run simulations
+for _ in range(num_repetitions):
+    ucb = UCB(n_arms=len(context_vectors))
+    exp3 = EXP3(n_arms=len(context_vectors))
+    linucb = LinUCB(n_arms=len(context_vectors), d=len(context_vectors[0]))
 
-    # EXP3
-    exp3_arm = exp3.select_arm()
-    exp3_context = context_vectors[exp3_arm]
-    exp3_reward = calculate_reward(t, exp3_context)
-    exp3.update(exp3_arm, exp3_reward, exp3_context)
-    if t == 1:
-        exp3_rewards.append(exp3_reward)
-    else:
-        exp3_rewards.append(exp3_rewards[-1] + exp3_reward)
+    ucb_cumulative_rewards = []
+    exp3_cumulative_rewards = []
+    linucb_cumulative_rewards = []
 
-    # LinUCB
-    linucb_arm = linucb.select_arm(context_vectors)
-    linucb_context = context_vectors[linucb_arm]
-    linucb_reward = calculate_reward(t, linucb_context)
-    linucb.update(linucb_arm, linucb_reward, linucb_context)
-    if t == 1:
-        linucb_rewards.append(linucb_reward)
-    else:
-        linucb_rewards.append(linucb_rewards[-1] + linucb_reward)
+    for t in range(1, num_iterations + 1):
+        # UCB
+        ucb_arm = ucb.select_arm()
+        ucb_context = context_vectors[ucb_arm]
+        ucb_reward = calculate_reward(t, ucb_context)
+        ucb.update(ucb_arm, ucb_reward)
+        if t == 1:
+            ucb_cumulative_rewards.append(ucb_reward)
+        else:
+            ucb_cumulative_rewards.append(ucb_cumulative_rewards[-1] + ucb_reward)
+
+        # EXP3
+        exp3_arm = exp3.select_arm()
+        exp3_context = context_vectors[exp3_arm]
+        exp3_reward = calculate_reward(t, exp3_context)
+        exp3.update(exp3_arm, exp3_reward, exp3_context)
+        if t == 1:
+            exp3_cumulative_rewards.append(exp3_reward)
+        else:
+            exp3_cumulative_rewards.append(exp3_cumulative_rewards[-1] + exp3_reward)
+
+        # LinUCB
+        linucb_arm = linucb.select_arm(context_vectors)
+        linucb_context = context_vectors[linucb_arm]
+        linucb_reward = calculate_reward(t, linucb_context)
+        linucb.update(linucb_arm, linucb_reward, linucb_context)
+        if t == 1:
+            linucb_cumulative_rewards.append(linucb_reward)
+        else:
+            linucb_cumulative_rewards.append(linucb_cumulative_rewards[-1] + linucb_reward)
+
+    # Accumulate rewards for averaging
+    ucb_rewards += np.array(ucb_cumulative_rewards)
+    exp3_rewards += np.array(exp3_cumulative_rewards)
+    linucb_rewards += np.array(linucb_cumulative_rewards)
+
+# Average rewards over repetitions
+ucb_rewards /= num_repetitions
+exp3_rewards /= num_repetitions
+linucb_rewards /= num_repetitions
 
 # Plot results
+plt.rcParams['lines.linewidth'] = 1
+plt.rcParams['lines.markersize'] = 0
 plt.figure(figsize=(12, 8))
 plt.plot(ucb_rewards, label='UCB')
 plt.plot(exp3_rewards, label='EXP3')
 plt.plot(linucb_rewards, label='LinUCB')
 plt.xlabel('Iteration')
 plt.ylabel('Cumulative Reward')
-plt.title('Cumulative Reward Over Time')
+plt.title('Average Cumulative Reward Over Time')
 plt.legend()
 plt.show()
 
